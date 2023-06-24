c44962c456b99a7af33789f80518fcdfa2495105
Prashant Kumar <43730425+03prashantpk@users.noreply.github.com>
2023-06-24 22:18:13 +0530

    pepTask1

---
+++ src/App.jsx
@@ -0,0 +1,199 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='card-outer'>
+
+      <div className="card">
+        <div className="card-inner">
+          <div className='card-head'>
+            <img src="https://cdn.shopify.com/s/files/1/0105/8881/5418/products/buydressesonlineindia-05554.jpg?v=1679388165" alt="product1" />
+            <p><i class="fal fa-heart"></i></p>
+          </div>
+
+          <div className="card-name-price">
+            <h3>White Traditional Long Dress</h3>
+            <p>$3.99</p>
+          </div>
+
+          <div className="card-color-size">
+            <div className='color'>
+              <button></button>
+              <button></button>
+              <button></button>
+            </div>
+
+            <div className='size'>
+              <button>S</button>
+              <button>M</button>
+              <button>L</button>
+            </div>
+          </div>
+
+          <div className="rating-buy">
+            <p>4B</p>
+            <p>Buy +</p>
+          </div>
+        </div>
+
+        <div className="card-footer">
+          <div className="coupon">
+            <p>Monday Happy</p>
+            <p>Monmpy</p>
+          </div>
+
+          <div className="offer">
+            <p>20%</p>
+            <p>Off</p>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="card">
+        <div className="card-inner">
+
+          <div className='card-head'>
+            <p><i class="fal fa-heart"></i></p>
+          </div>
+
+          <div className="card-name-price">
+            <h3>White Traditional Long Dress</h3>
+            <p>$3.99</p>
+          </div>
+
+          <div className="card-color-size">
+            <div className='color'>
+              <button></button>
+              <button></button>
+              <button></button>
+            </div>
+
+            <div className='size'>
+              <button>S</button>
+              <button>M</button>
+              <button>L</button>
+            </div>
+          </div>
+
+          <div className="rating-buy">
+            <p>4B</p>
+            <p>Buy +</p>
+          </div>
+        </div>
+
+        <div className="card-footer">
+          <div className="coupon">
+            <p>Monday Happy</p>
+            <p>Monmpy</p>
+          </div>
+
+          <div className="offer">
+            <p>20%</p>
+            <p>Off</p>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="card">
+        <div className="card-inner">
+
+          <div className='card-head'>
+            <p><i class="fal fa-heart"></i></p>
+          </div>
+
+          <div className="card-name-price">
+            <h3>White Traditional Long Dress</h3>
+            <p>$3.99</p>
+          </div>
+
+          <div className="card-color-size">
+            <div className='color'>
+              <button></button>
+              <button></button>
+              <button></button>
+            </div>
+
+            <div className='size'>
+              <button>S</button>
+              <button>M</button>
+              <button>L</button>
+            </div>
+          </div>
+
+          <div className="rating-buy">
+            <p>4B</p>
+            <p>Buy +</p>
+          </div>
+        </div>
+
+        <div className="card-footer">
+          <div className="coupon">
+            <p>Monday Happy</p>
+            <p>Monmpy</p>
+          </div>
+
+          <div className="offer">
+            <p>20%</p>
+            <p>Off</p>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="card">
+        <div className="card-inner">
+
+          <div className='card-head'>
+            <img src="" alt="" />
+            <p><i class="fal fa-heart"></i></p>
+          </div>
+
+          <div className="card-name-price">
+            <h3>White Traditional Long Dress</h3>
+            <p>$3.99</p>
+          </div>
+
+          <div className="card-color-size">
+            <div className='color'>
+              <button></button>
+              <button></button>
+              <button></button>
+            </div>
+
+            <div className='size'>
+              <button>S</button>
+              <button>M</button>
+              <button>L</button>
+            </div>
+          </div>
+
+          <div className="rating-buy">
+            <p>4B</p>
+            <p>Buy +</p>
+          </div>
+        </div>
+
+        <div className="card-footer">
+          <div className="coupon">
+            <p>Monday Happy</p>
+            <p>Monmpy</p>
+          </div>
+
+          <div className="offer">
+            <p>20%</p>
+            <p>Off</p>
+          </div>
+        </div>
+
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default App
